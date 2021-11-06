dc098df8ce9aa7a64d133da13993875a92cc65ea
nabil <merchant.nabil@gmail.com>
2021-11-05 23:57:42 -0500

    dictionary comprehension more examples

--- main.py
+++ main.py
@@ -55,10 +55,28 @@
 # Dictionary comprehension
 # new_dict = {new_key:new_value for item in list}
 # new_dict = {new_key:new_value for (key,value) in dict.items()}
-import random
+# import random
+# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+# student_scores = {name:random.randint(0, 100) for name in names}
+# print(student_scores)
+# passed_students = {name:score for (name, score) in student_scores.items() if score >= 60}
+# print(passed_students)
 
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-student_scores = {name:random.randint(0, 100) for name in names}
-print(student_scores)
-passed_students = {name:score for (name, score) in student_scores.items() if score >= 60}
-print(passed_students)
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# # Don't change code above 👆
+# result = {word:len(word) for word in sentence.split(" ")}
+# print(result)
+
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+weather_f = {key:value*9/5 + 32 for key, value in weather_c.items()}
+print(weather_f)
